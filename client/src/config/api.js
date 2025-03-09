f88e9332e2c1e33dc62edeb87b0be152d7f1016d
amitpaul4006 <amitpaul4006@gmail.com>
2025-03-09 17:33:32 +0530

    Updated environment configurations

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,8 @@
-const API_URL = '/api';  // Simplified to always use /api
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? '/.netlify/functions/api'
+  : 'http://localhost:5000/api';
+
+console.log('Environment:', process.env.NODE_ENV);
 console.log('Using API URL:', API_URL);
 
 export default API_URL;
